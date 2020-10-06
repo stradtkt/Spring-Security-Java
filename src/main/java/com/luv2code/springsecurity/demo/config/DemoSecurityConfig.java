458a2bcd89b61554ee7f74c7c2840c397a89fc0e
Kevin Stradtman <stradtkt22@gmail.com>
2020-10-06 06:29:17 -0400

    Able to get a login form from spring security

--- src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,9 +1,12 @@
 package com.luv2code.springsecurity.demo.config;
 
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
 
 @Configuration
 @EnableWebSecurity
@@ -11,7 +14,12 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		super.configure(auth);
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		auth.inMemoryAuthentication()
+			.withUser(users.username("patrick").password("password").roles("MANAGER"))
+			.withUser(users.username("mary").password("test123").roles("EMPLOYEE"))
+			.withUser(users.username("susan").password("password123").roles("MANAGER"))
+			.withUser(users.username("kevin").password("password1234").roles("ADMIN"));
 	}
 	
 }
