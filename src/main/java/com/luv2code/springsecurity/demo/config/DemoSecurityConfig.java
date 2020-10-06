d36ef7ea9c4d5acf3dc1343879df4afbf843dad2
Kevin Stradtman <stradtkt22@gmail.com>
2020-10-06 07:25:57 -0400

    Added the config for the custom login form

--- src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -3,6 +3,7 @@ package com.luv2code.springsecurity.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -15,11 +16,24 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		UserBuilder users = User.withDefaultPasswordEncoder();
-		auth.inMemoryAuthentication()
-			.withUser(users.username("patrick").password("password").roles("MANAGER"))
-			.withUser(users.username("mary").password("test123").roles("EMPLOYEE"))
-			.withUser(users.username("susan").password("password123").roles("MANAGER"))
-			.withUser(users.username("kevin").password("password1234").roles("ADMIN"));
+		auth.inMemoryAuthentication().withUser(users.username("patrick").password("password").roles("MANAGER"));
+		auth.inMemoryAuthentication().withUser(users.username("mary").password("test123").roles("EMPLOYEE"));
+		auth.inMemoryAuthentication().withUser(users.username("susan").password("password123").roles("MANAGER"));
+		auth.inMemoryAuthentication().withUser(users.username("kevin").password("password1234").roles("ADMIN"));
 	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.anyRequest()
+			.authenticated()
+			.and()
+			.formLogin()
+			.loginPage("/loginPage")
+			.loginProcessingUrl("/authenticateUser")
+			.permitAll();
+	}
+	
+	
 	
 }
